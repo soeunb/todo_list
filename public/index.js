fac8e308a9c93e9919bbdc2e54e8661129b192e0
soeunb <b_soeun_@naver.com>
2019-12-21 04:39:16 +0900

    curTime 해결 전

--- public/index.js
+++ public/index.js
@@ -1,46 +1,65 @@
-var today = new Date();
-var week = new Array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
-var month = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
 var todayText = document.querySelector(".today-text");
-todayText.innerHTML = week[today.getDay()] + ", " + today.getDate() + " " + month[today.getMonth()];
+function time() {
+    var today = new Date();
+    var week = new Array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
+    var month = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+    todayText.innerHTML = week[today.getDay()] + ", " + today.getDate() + " " + month[today.getMonth()];
+    todayText.setAttribute("data-cur-time", today.toString());
+    setTimeout("time()", 1000);
+}
+window.onload = function () {
+    time();
+};
 var toDoForm = document.querySelector(".todo-form"), toDoInput = toDoForm.querySelector("textarea"), toDoAddBtn = document.querySelector(".add-btn"), toDoList = document.querySelector(".todo-list");
 var TODO_STORAGE = "todo";
 var todos = [];
+function re() {
+}
 function saveTodo() {
     localStorage.setItem(TODO_STORAGE, JSON.stringify(todos));
 }
-function addTodo(text) {
+function addTodo(text, curTime) {
     var li = document.createElement("li");
     var idx = todos.length + 1;
     var comBtn = document.createElement("button");
     var comImg = document.createElement("img");
-    var delBtn = document.createElement("button");
-    var delImg = document.createElement("img");
+    /*
+    const delBtn = document.createElement("button");
+    const delImg = document.createElement("img");
+    */
     comBtn.setAttribute("type", "button");
     comBtn.setAttribute("class", "com-btn");
     comImg.setAttribute("src", "./img/c_btn.png");
     comBtn.appendChild(comImg);
+    /*
     delBtn.setAttribute("type", "button");
     delBtn.setAttribute("class", "del-btn");
     delImg.setAttribute("src", "./img/x_btn.png");
     delBtn.appendChild(delImg);
     delBtn.addEventListener("click", deleteTodo);
+    */
     var p = document.createElement("p");
     var span = document.createElement("span");
+    var spanTime = document.createElement("span");
+    spanTime.setAttribute("class", "li-time");
     p.appendChild(span);
+    p.appendChild(spanTime);
     span.innerText = text;
+    spanTime.innerText = "(" + todayText.getAttribute("data-cur-time").split(" GMT")[0] + ")";
     var div = document.createElement("div");
     div.setAttribute("class", "li-btn");
     div.appendChild(comBtn);
-    div.appendChild(delBtn);
+    //div.appendChild(delBtn);
     li.appendChild(p);
     li.appendChild(div);
     li.id = idx;
     toDoList.appendChild(li);
     var todoObj = {
         text: text,
-        idx: idx
+        idx: idx,
+        curTime: curTime
     };
+    console.log(curTime);
     todos.push(todoObj);
     saveTodo();
 }
@@ -55,10 +74,12 @@ function deleteTodo(event) {
     todos = reDelTodo;
     saveTodo();
 }
+// 추가 버튼 클릭 시
 toDoAddBtn.addEventListener("click", function () {
-    var currentValue = toDoInput.value; //toDoForm.querySelector("#~")로 했을 때 toDoInput.nodevalue?
-    if (currentValue.replace(/^\s/gm, '') !== "") {
-        addTodo(currentValue);
+    var inputVal = toDoInput.value; //toDoForm.querySelector("#~")로 했을 때 toDoInput.nodevalue?
+    var curTime = todayText.getAttribute("data-cur-time");
+    if (inputVal.replace(/^\s/gm, '') !== "") {
+        addTodo(inputVal, curTime);
         toDoInput.value = "";
     }
     else {
@@ -70,7 +91,8 @@ function loadTodo() {
     if (storedTodo !== null) {
         var parsedTodo = JSON.parse(storedTodo);
         parsedTodo.forEach(function (todo) {
-            addTodo(todo.text);
+            //const loadCurTime = todo.curTime.toUTCString();          
+            addTodo(todo.text, todo.curTime);
         });
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-const today = new Date();
-const week = new Array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
-const month = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
 const todayText = document.querySelector(".today-text");
 
-todayText.innerHTML = week[today.getDay()] + ", " + today.getDate() + " " + month[today.getMonth()];
+function time(){
+    const today = new Date();
+    const week = new Array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
+    const month = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
+    todayText.innerHTML = week[today.getDay()] + ", " + today.getDate() + " " + month[today.getMonth()];
+    todayText.setAttribute("data-cur-time",today.toString());
+    setTimeout("time()", 1000);
+}
+
+window.onload = function(){
+    time();
+}
 
 interface TodoObject {
     todoText: string;
-    todoIdx: number;
+    todoIdx: any;
 }
 
 const toDoForm = document.querySelector(".todo-form"),
@@ -19,37 +27,46 @@ const TODO_STORAGE = "todo";
 
 let todos:any[] = [];
 
+function re(){
+    
+}
+
 function saveTodo(){
     localStorage.setItem(TODO_STORAGE, JSON.stringify(todos));
 }
 
-function addTodo(text:string){
+function addTodo(text:string, curTime:any){
     const li = document.createElement("li");
     const idx:any = todos.length + 1;
-
     const comBtn = document.createElement("button");
     const comImg = document.createElement("img");
+    /*
     const delBtn = document.createElement("button");
     const delImg = document.createElement("img");
+    */
     comBtn.setAttribute("type", "button");
     comBtn.setAttribute("class", "com-btn");
     comImg.setAttribute("src", "./img/c_btn.png");
     comBtn.appendChild(comImg);
+    /*
     delBtn.setAttribute("type", "button");
     delBtn.setAttribute("class", "del-btn");
     delImg.setAttribute("src", "./img/x_btn.png");
     delBtn.appendChild(delImg);
     delBtn.addEventListener("click", deleteTodo);
-
+    */
     const p = document.createElement("p");
     const span = document.createElement("span");
+    const spanTime = document.createElement("span");
+    spanTime.setAttribute("class", "li-time");
     p.appendChild(span);
+    p.appendChild(spanTime);
     span.innerText = text;
-
+    spanTime.innerText = "("+todayText.getAttribute("data-cur-time").split(" GMT")[0]+")";
     const div = document.createElement("div");
     div.setAttribute("class", "li-btn");
     div.appendChild(comBtn);
-    div.appendChild(delBtn);
+    //div.appendChild(delBtn);
     li.appendChild(p);
     li.appendChild(div);
     li.id = idx;
@@ -57,8 +74,10 @@ function addTodo(text:string){
 
     const todoObj = {
         text: text,
-        idx: idx
+        idx: idx,
+        curTime : curTime
     };
+    console.log(curTime);
     todos.push(todoObj);
     saveTodo();
 }
@@ -75,10 +94,12 @@ function deleteTodo(event:any){
     saveTodo();
 }
 
+// 추가 버튼 클릭 시
 toDoAddBtn.addEventListener("click", function () {
-    const currentValue = toDoInput.value; //toDoForm.querySelector("#~")로 했을 때 toDoInput.nodevalue?
-    if(currentValue.replace(/^\s/gm, '') !== ""){
-        addTodo(currentValue);
+    const inputVal = toDoInput.value; //toDoForm.querySelector("#~")로 했을 때 toDoInput.nodevalue?
+    const curTime = todayText.getAttribute("data-cur-time");
+    if(inputVal.replace(/^\s/gm, '') !== ""){
+        addTodo(inputVal, curTime);
         toDoInput.value = "";
     }else{
         alert("할 일을 입력하세요");
@@ -90,7 +111,8 @@ function loadTodo(){
     if(storedTodo !== null){
         const parsedTodo = JSON.parse(storedTodo);
         parsedTodo.forEach(function(todo:any){
-            addTodo(todo.text);
+            //const loadCurTime = todo.curTime.toUTCString();          
+            addTodo(todo.text, todo.curTime);
         });
     }
 }
